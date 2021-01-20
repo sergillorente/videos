ed0be8c9e3ab3767e4af2fc5e8d4d5274c70c5b6
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-01-20 18:58:44 +0100

    List of videos styled with semantinc ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+
+const KEY = 'AIzaSyB24BQTI61f57AgQ9ZsR3b-dUEvUshWxPA';
 
 class App extends React.Component {
+    state = {
+        videos: []
+    };
+
+    onTermSubmit = async term => {
+        const response = await youtube.get("/search", {
+            params: {
+                q: term, 
+                part: "snippet",
+                type: 'video',
+                maxResults: 5,
+                key: KEY
+            }
+        });
+        this.setState({ videos: response.data.items });
+    };
+
+
     render () {
         return (
         <div className="ui container">
-            <SearchBar />
+            <SearchBar onFormSubmit={this.onTermSubmit} />
+            <VideoList videos={this.state.videos} />
         </div>
         )
     }
